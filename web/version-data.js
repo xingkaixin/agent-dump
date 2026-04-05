ad7c9418ee4f0868b1be4907942f51e83efcf73e
XingKaiXin <xingkaixin@gmail.com>
2026-04-05 10:13:39 +0800

    chore: bump version to 0.6.18

--- web/version-data.js
+++ web/version-data.js
@@ -1,5 +1,5 @@
 window.AGENT_DUMP_WEB_DATA = {
-  "version": "0.6.17",
+  "version": "0.6.18",
   "changelogUrl": {
     "en": "https://github.com/xingkaixin/agent-dump/blob/main/CHANGELOG.md",
     "zh": "https://github.com/xingkaixin/agent-dump/blob/main/docs/zh/CHANGELOG.md"
